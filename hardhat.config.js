37003f92280f90c5a6e09f3dc040d14cc901f084
Arsenii Ovsianykov <arsen39@gmail.com>
2022-10-05 16:04:03 +0300

    Update dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-tracer");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {
+      
+    }
+  },
+  mocha: {
+    timeout: 100000000,
+  }
 };
