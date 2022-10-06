64e11b55b7506bbbf5721244f3da8ebd3bed6309
Arsenii Ovsianykov <arsen39@gmail.com>
2022-10-06 17:40:58 +0300

    Flow update

--- contracts/escrow/Escrow.sol
+++ contracts/escrow/Escrow.sol
@@ -2,14 +2,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "../lib/TransferHelper.sol";
 
 contract Escrow is
     Initializable,
-    OwnableUpgradeable,
-    ReentrancyGuardUpgradeable
+    OwnableUpgradeable
 {
+    uint256 public PERIOD_FOR_RESOLVE = 259200;
     uint256 public tradesCounter;
     address public auzToken;
     address public auzWallet;
@@ -26,18 +25,24 @@ contract Escrow is
         address buyer;
         uint256 price;
         uint256 fee;
-        bool registered;
-        bool confirmed;
+        uint256 timestamp;
         bool paid;
         bool approved;
         bool finished;
     }
 
-    event TradeRegistered();
-    event BuyerConfirmed();
-    event TradePaid();
-    event TradeApproved();
-    event TradeFinished();
+    event TradeRegistered(
+        string indexed tradeId,
+        bytes tradeHash,
+        address seller,
+        address buyer,
+        uint256 price,
+        uint256 fee
+    );
+    event TradePaid(string tradeId, uint256 amount);
+    event TradeApproved(string tradeId);
+    event TradeFinished(string tradeId);
+    event TradeResolved(string tradeId, bool result, string reason);
 
     receive() external payable {}
 
@@ -46,7 +51,6 @@ contract Escrow is
         initializer
     {
         __Ownable_init();
-        __ReentrancyGuard_init();
         managers[msg.sender] = true;
         auzToken = _auzToken;
         auzWallet = _auzWallet;
@@ -56,6 +60,7 @@ contract Escrow is
         string memory _tradeId,
         bytes memory _tradeHash,
         address _seller,
+        address _buyer,
         uint256 _price,
         uint256 _fee
     ) external {
@@ -70,26 +75,15 @@ contract Escrow is
         trade.tradeId = _tradeId;
         trade.tradeHash = _tradeHash;
         trade.seller = _seller;
+        trade.buyer = _buyer;
         trade.price = _price;
         trade.fee = _fee;
-        trade.registered = true;
+        trade.timestamp = block.timestamp;
+
+        emit TradeRegistered (_tradeId, _tradeHash, _seller, _buyer, _price, _fee);
     }
 
-    function confirmBuyer(string memory _tradeId, address _buyer) external {
-        require(managers[msg.sender] == true, "Escrow: Action is not allowed");
-        require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
-        Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
-        require(trade.registered, "Escrow: Trade is not exist");
-        require(!trade.confirmed, "Escrow: Buyer is already confirmed");
-        require(
-            trade.buyer == address(0),
-            "Escrow: Buyer is already confirmed"
-        );
-        trade.buyer = _buyer;
-        trade.confirmed = true;
-    }
-
-    function payForTrade(string memory _tradeId) external {
+    function payTrade(string memory _tradeId) external {
         require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
         Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
         require(trade.buyer != address(0), "Escrow: Buyer is anot confirmed");
@@ -101,23 +95,48 @@ contract Escrow is
             trade.price + trade.fee
         );
         trade.paid = true;
+
+        emit TradePaid (_tradeId, trade.price + trade.fee);
     }
 
     function approveTrade(string memory _tradeId) external {
-        require(managers[msg.sender] == true, "Escrow: Action is not allowed");
         require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
         Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
+        require(trade.buyer == msg.sender, "Escrow: You are not a buyer");
         require(trade.paid, "Escrow: Trade is not paid");
         trade.approved = true;
+
+        emit TradeApproved (_tradeId);
     }
 
     function finishTrade(string memory _tradeId) external {
+        require(managers[msg.sender] == true, "Escrow: Action is not allowed");
         require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
         Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
+        require(!trade.finished, "Escrow: Trade is finished");
         require(trade.approved, "Escrow: Trade is not approved");
-        require(trade.buyer == msg.sender, "Escrow: You are not a buyer");
         TransferHelper.safeTransfer(auzToken, trade.seller, trade.price);
         TransferHelper.safeTransfer(auzToken, auzWallet, trade.fee);
         trade.finished = true;
+
+        emit TradeFinished (_tradeId);
+    }
+
+    function resolveTrade(string memory _tradeId, bool _result, string memory _reason) external {
+        require(managers[msg.sender] == true, "Escrow: Action is not allowed");
+        require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
+        Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
+        require(!trade.finished, "Escrow: Trade is finished");
+        require(block.timestamp >= trade.timestamp + PERIOD_FOR_RESOLVE, "Escrow: To early to resolve");
+        
+        if (_result) {
+            TransferHelper.safeTransfer(auzToken, trade.seller, trade.price);
+            TransferHelper.safeTransfer(auzToken, auzWallet, trade.fee);
+        } else {
+             TransferHelper.safeTransfer(auzToken, trade.buyer, trade.price + trade.fee);
+        }
+        trade.finished = true;
+
+        emit TradeResolved (_tradeId, _result, _reason);
     }
 }
