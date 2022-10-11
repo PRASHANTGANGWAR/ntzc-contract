b3f8c8874032a88829f2fb8e29e472f253c94254
Arsenii Ovsianykov <arsen39@gmail.com>
2022-10-11 16:07:38 +0300

    Beautify the escrow contract

--- contracts/escrow/Escrow.sol
+++ contracts/escrow/Escrow.sol
@@ -5,15 +5,23 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "../lib/TransferHelper.sol";
 
 contract Escrow is Initializable, OwnableUpgradeable {
+    // The time elapsed since the registration of the trade, after which the administrator will be able to resolve the trade
     uint256 public PERIOD_FOR_RESOLVE;
+    // The counter of trades
     uint256 public tradesCounter;
+    // Address of AZK token
     address public auzToken;
+    // Address of wallet for receiving fees
     address public auzWallet;
 
+    // Mapping of admins addresses
     mapping(address => bool) managers;
+    // Mapping of trades
     mapping(uint256 => Trade) public trades;
+    // Mapping of trade's external ids to internal ids
     mapping(string => uint256) public tradesIdsToTrades;
 
+    // The struct of trade
     struct Trade {
         string tradeId;
         bytes tradeHash;
@@ -27,6 +35,7 @@ contract Escrow is Initializable, OwnableUpgradeable {
         bool finished;
     }
 
+    // Events
     event TradeRegistered(
         string indexed tradeId,
         bytes tradeHash,
@@ -44,6 +53,11 @@ contract Escrow is Initializable, OwnableUpgradeable {
         revert("Escrow: Contract cannot work with ETH");
     }
 
+    /**
+     * @dev Initializes the contract
+     * @param _auzToken Address of AZK token
+     * @param _auzWallet Address of wallet for receiving fees
+     */
     function initialize(address _auzToken, address _auzWallet)
         public
         initializer
@@ -55,16 +69,37 @@ contract Escrow is Initializable, OwnableUpgradeable {
         PERIOD_FOR_RESOLVE = 259200;
     }
 
-    function changeManager(address _manager, bool _isManager) external onlyOwner {
+    /**
+     * @dev Changes admin permissions for the address (for owner only)
+     * @param _manager Address of admin
+     * @param _isManager Status of admin (Is admin or not)
+     */
+    function changeManager(address _manager, bool _isManager)
+        external
+        onlyOwner
+    {
         require(_manager != address(0), "Escrow: Zero address is not allowed");
         managers[_manager] = _isManager;
     }
 
+    /**
+     * @dev Changes address of wallet for receiving fees (for owner only)
+     * @param _wallet Address of wallet for receiving fees
+     */
     function changeWallet(address _wallet) external onlyOwner {
         require(_wallet != address(0), "Escrow: Zero address is not allowed");
         auzWallet = _wallet;
     }
 
+    /**
+     * @dev Registers new trade (only for admin)
+     * @param _tradeId External trade id
+     * @param _tradeHash Hash of trade data
+     * @param _seller Address of seller
+     * @param _buyer Address of buyer
+     * @param _price Price of trade
+     * @param _fee Fee of trade
+     */
     function registerTrade(
         string memory _tradeId,
         bytes memory _tradeHash,
@@ -99,6 +134,10 @@ contract Escrow is Initializable, OwnableUpgradeable {
         );
     }
 
+    /**
+     * @dev Pays for trade (only for buyer)
+     * @param _tradeId External trade id
+     */
     function payTrade(string memory _tradeId) external {
         require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
         Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
@@ -115,6 +154,10 @@ contract Escrow is Initializable, OwnableUpgradeable {
         emit TradePaid(_tradeId, trade.price + trade.fee);
     }
 
+    /**
+     * @dev Approves trade (only for buyer)
+     * @param _tradeId External trade id
+     */
     function approveTrade(string memory _tradeId) external {
         require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
         Trade storage trade = trades[tradesIdsToTrades[_tradeId]];
@@ -125,6 +168,10 @@ contract Escrow is Initializable, OwnableUpgradeable {
         emit TradeApproved(_tradeId);
     }
 
+    /**
+     * @dev Finishes trade (only for admin)
+     * @param _tradeId External trade id
+     */
     function finishTrade(string memory _tradeId) external {
         require(managers[msg.sender] == true, "Escrow: Action is not allowed");
         require(tradesIdsToTrades[_tradeId] != 0, "Escrow: Trade is not exist");
@@ -138,6 +185,12 @@ contract Escrow is Initializable, OwnableUpgradeable {
         emit TradeFinished(_tradeId);
     }
 
+    /**
+     * @dev Resolves trade (only for admin). Uses for resolving disputes.
+     * @param _tradeId External trade id
+     * @param _result Result of trade
+     * @param _reason Reason of trade
+     */
     function resolveTrade(
         string memory _tradeId,
         bool _result,
@@ -167,6 +220,10 @@ contract Escrow is Initializable, OwnableUpgradeable {
         emit TradeResolved(_tradeId, _result, _reason);
     }
 
+    /**
+     * @dev Gets trade by external trade id
+     * @param _tradeId External trade id
+     */
     function getTrade(string memory _tradeId)
         external
         view
