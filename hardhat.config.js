6a6f053f3f65b9ad77e6a70084a8cef6675bb0a8
Arsenii Ovsianykov <arsen39@gmail.com>
2022-10-19 18:07:08 +0300

    Config updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,11 +28,11 @@ module.exports = {
     hardhat: {
       forking: {
         // ** ETH **
-        url: "https://eth-mainnet.g.alchemy.com/v2/ykAnFwdjMGr5J4O_ZINye-fbEO_SHFUe",
+        url: process.env.ETH_FORK_URL,
         // ** BSC **
-        // url: "https://powerful-rough-pool.bsc.discover.quiknode.pro/61d092c01e8a3e12dcc314cdcb85aecdc1f09ecb/",
+        // url: process.env.BSC_FORK_URL,
         // ** POL **
-        // url: "https://polygon-mainnet.infura.io/v3/a52089cc61bc43a9ab54d46e82e5933e",
+        // url: process.env.POL_FORK_URL,
         // ** BSC TESTNET **
         // url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       }
